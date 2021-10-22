e0820b86e50ee908668baa245b2a0ef836f5ee30
seilylook <seilylook@naver.com>
2021-10-22 18:27:59 +0900

    feat: contentsContainer 구현

---
+++ fixtures/contents.js
@@ -0,0 +1,11 @@
+const contents = [
+  { id: 1, name: '창세기' },
+  { id: 2, name: '시편' },
+  { id: 3, name: '마태복음' },
+  { id: 4, name: '마가복음' },
+  { id: 5, name: '누가복음' },
+  { id: 6, name: '요한복음' },
+  { id: 7, name: '베드로서' },
+];
+
+export default contents;
