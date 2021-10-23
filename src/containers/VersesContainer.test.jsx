a0c9bc37d1e65142bbbed85c01aad1efc9381372
seilylook <seilylook@naver.com>
2021-10-23 12:24:33 +0900

    feat: 성경 구절 보여주는 페이지 구현

---
+++ src/containers/VersesContainer.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import VersesContainer from './VersesContainer';
+
+describe('VersesContainer', () => {
+  it('renders verses', () => {
+    useSelector.mockImplementation((selector) => selector({
+      verses: [
+        { id: 1, phrase: '구절 테스트' },
+      ],
+    }));
+
+    const handleClick = jest.fn();
+
+    const { container } = render((
+      <MemoryRouter>
+        <VersesContainer onClickVerse={handleClick} />
+      </MemoryRouter>
+    ));
+
+    expect(container).toHaveTextContent('구절 테스트');
+  });
+});
