a9a3b518c672abe8ccf7d1514f8681bacf547184
seilylook <seilylook@naver.com>
2021-10-27 15:18:41 +0900

    fix: login style 수정

--- src/components/login/LogoutForm.jsx
+++ src/components/login/LogoutForm.jsx
@@ -1,56 +1,31 @@
 import styled from '@emotion/styled';
-import mediaquery from '../../styles/mediaquery';
+import colors from '../../styles/colors';
 
-const LoginBox = styled.div(mediaquery({
-  width: ['12em', '12em', '15em', '23em', '27em', '30em'],
-  margin: '0 auto',
-  padding: '0.5em',
-  textAlign: 'center',
-  '& div': {
-    height: ['3em', '3em', '3em', '4em', '5em', '5em'],
-    margin: [
-      '3em auto',
-      '2.5em auto',
-      '2.2em auto',
-      '2.8em auto',
-      '3.5em auto',
-      '4em auto',
-    ],
-    '& p': {
-      margin: '0.3em auto',
-      fontSize: ['0.8em', '0.8em', '0.9em', '1.2em', '1.4em', '1.5em'],
-    },
-  },
-}));
-
-const Button = styled.button(mediaquery({
-  display: 'block',
-  width: ['15.5em', '15em', '17.5em', '18.5em', '21.5em', '21em'],
-  height: ['1.5em', '1.5em', '1.6em', '1.5em', '1.7em', '1.8em'],
+const Button = styled.div({
+  width: '16em',
+  height: '1em',
   margin: '1em auto 0.5em',
-  padding: '0.5em 0',
   borderRadius: '0.2em',
-  backgroundColor: 'white',
-  fontSize: ['0.7em', '0.7em', '0.8em', '1.2em', '1.2em', '1.4em'],
-  fontWeight: '500',
+  backgroundColor: `${colors.button}`,
+  fontsize: '0.7em',
   cursor: 'pointer',
   lineHeight: 1,
   '&:hover': {
-    color: 'white',
-    backgroundColor: 'blue',
+    color: `${colors.white}`,
+    backgroundColor: `${colors.blue_text}`,
     fontWeight: '600',
   },
-}));
+});
 
 export default function LogoutForm({ onClick }) {
   return (
-    <LoginBox>
+    <>
       <Button
         type="button"
         onClick={onClick}
       >
         로그아웃
       </Button>
-    </LoginBox>
+    </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,54 +3,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styled from '@emotion/styled';
-import mediaquery from '../styles/mediaquery';
 
-const List = styled.ul(mediaquery({
+const List = styled.ul({
   display: 'flex',
   margin: 0,
-  padding: 0,
+  padding: '2em',
   listStyle: 'none',
-  width: '100%',
-}));
+  justifyContent: 'center',
+});
 
-const Item = styled.li(mediaquery({
-  width: ['5em', '5em', '5em', '5em', '5em', '5em'],
-  height: ['2em', '2em', '2em', '2.4em', '2.5em'],
-  margin: '0 2em 0 auto',
-  padding: '0.5em',
-  borderRadius: '0.4em',
-  backgroundColor: 'rgb(180, 251, 255)',
-  fontSize: ['0.7em', '0.8em', '0.9em', '1em', '1.2em', '1.5em'],
-  fontWeight: '500',
-  lineHeight: [1, 1, 1, 1.2, 1.5, 1.5],
-  textAlign: 'center',
+const Item = styled.li({
+  marginRight: '2.5em',
   '& a': {
-    color: 'black',
+    color: '#333',
+    fontSize: '1.6em',
+    textDecoration: 'none',
     '&:hover': {
-      color: 'black',
-      fontWeight: '700',
+      fontWeight: 'bold',
+      color: '#000',
     },
   },
-}));
+});
 
 export default function HomePage() {
   return (
-    <List>
-      <Item>
-        <Link to="/register">회원가입</Link>
-      </Item>
-      <Item>
-        <Link to="/login">로그인</Link>
-      </Item>
-      <Item>
-        <Link to="/verses">성경 구절</Link>
-      </Item>
-      <Item>
-        <Link to="/diaryInput">기도 노트</Link>
-      </Item>
-      <Item>
-        <Link to="/diaries">노트 목록</Link>
-      </Item>
-    </List>
+    <>
+      <List>
+        <Item>
+          <Link to="/register">회원가입</Link>
+        </Item>
+        <Item>
+          <Link to="/login">로그인</Link>
+        </Item>
+        <Item>
+          <Link to="/verses">성경 구절</Link>
+        </Item>
+        <Item>
+          <Link to="/diaryInput">기도 노트</Link>
+        </Item>
+        <Item>
+          <Link to="/diaries">게시판</Link>
+        </Item>
+      </List>
+    </>
   );
 }
