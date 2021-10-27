122ecf756c04f69f49c4f1a62570174391ee2319
seilylook <seilylook@naver.com>
2021-10-27 16:00:11 +0900

    fix: DiaryInput Page css 추가

--- src/pages/DiaryInputPage.jsx
+++ src/pages/DiaryInputPage.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+import colors from '../styles/colors';
+
 import DiaryInputContainer from '../containers/DiaryInputContainer';
+
+const Container = styled.div({
+  width: '22em',
+  height: '24em',
+  margin: '4em auto 0',
+  padding: 0,
+  borderRadius: '0.5em',
+  textAlign: 'center',
+  background: `${colors.diaryInput_background}`,
+});
+
+const Title = styled.h2({
+  display: 'inline-block',
+  width: '14em',
+  height: '1em',
+  margin: '1em auto',
+  fontsize: '1.8em',
+  textAlign: 'center',
+  letterSpacing: '0.2em',
+});
 
 export default function DiaryInputPage() {
   return (
-    <div>
-      <h2>기도 노트</h2>
+    <Container>
+      <Title>기도 노트</Title>
       <DiaryInputContainer />
-    </div>
+    </Container>
   );
 }
