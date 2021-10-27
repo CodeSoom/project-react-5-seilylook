f1036d618ca78ccf5e693843a62309611fcc3b08
seilylook <seilylook@naver.com>
2021-10-27 17:10:37 +0900

    fix: loginPage && registerPage css 수정 - mediaQuery

--- src/components/login/LogoutForm.jsx
+++ src/components/login/LogoutForm.jsx
@@ -1,21 +1,23 @@
 import styled from '@emotion/styled';
 import colors from '../../styles/colors';
+import mediaquery from '../../styles/mediaquery';
 
-const Button = styled.div({
-  width: '16em',
-  height: '1em',
+const Button = styled.div(mediaquery({
+  width: ['16em', '15.5em', '20em', '22em', '22em', '21em'],
+  height: ['1em', '1.2em', '1.2em', '1.5em', '1.5em', '1.8em'],
   margin: '1em auto 0.5em',
   borderRadius: '0.2em',
   backgroundColor: `${colors.button}`,
-  fontsize: '0.7em',
+  fontSize: ['0.7em', '0.7em', '0.7em', '1em', '1.2em', '1.4em'],
   cursor: 'pointer',
   lineHeight: 1,
   '&:hover': {
     color: `${colors.white}`,
     backgroundColor: `${colors.blue_text}`,
     fontWeight: '600',
+    textAlign: 'center',
   },
-});
+}));
 
 export default function LogoutForm({ onClick }) {
   return (
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,28 +2,35 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 import colors from '../styles/colors';
+import mediaquery from '../styles/mediaquery';
 
 import RegisterFormContainer from '../containers/RegisterFormContainer';
 
-const Container = styled.div({
-  width: '22em',
-  height: '24em',
-  margin: '4em auto 0',
-  padding: 0,
+const Container = styled.div(mediaquery({
+  width: ['14em', '14em', '18em', '28em', '30em', '40em'],
+  height: ['35.5em', '36em', '40em', '40.5em', '43em', '47em'],
+  margin: [
+    '3em auto',
+    '3em auto',
+    '2em auto',
+    '6em auto',
+    '10em auto',
+    '15em auto 0',
+  ],
   borderRadius: '0.5em',
   textAlign: 'center',
   background: `${colors.login_background}`,
-});
+}));
 
-const Title = styled.h2({
+const Title = styled.h2(mediaquery({
   display: 'inline-block',
-  width: '14em',
-  height: '1em',
+  width: ['14em', '14em', '16.4em', '18.6em', '16.6em', '20em'],
+  height: ['1.2em', '1.3em', '1.3m', '1.3em', '1.2em', '1.3em'],
   margin: '1em auto',
-  fontsize: '1.8em',
+  fontSize: ['1em', '1em', '1.1em', '1.5em', '1.8em', '2em'],
   textAlign: 'center',
   letterSpacing: '0.2em',
-});
+}));
 
 export default function RegisterPage() {
   return (
