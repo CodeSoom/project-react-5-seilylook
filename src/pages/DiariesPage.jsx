b38f632c05fa8ae980cf520632b349adcdcccfb6
seilylook <seilylook@naver.com>
2021-10-28 09:21:04 +0900

    fix: 기도 노트 작성 페이지 수정

--- src/pages/DiariesPage.jsx
+++ src/pages/DiariesPage.jsx
@@ -1,7 +1,28 @@
 /* eslint-disable react/jsx-no-bind */
 import { useHistory } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+import colors from '../styles/colors';
+import mediaquery from '../styles/mediaquery';
+
 import DiariesContainer from '../containers/DiariesContainer';
+
+const Container = styled.div(mediaquery({
+  width: ['17em', '17em', '21em', '31em', '33em', '38em'],
+  height: ['16em', '15em', '15em', '20em', '25em', '28em'],
+  margin: [
+    '4em auto 0',
+    '4em auto 0',
+    '5em auto 0',
+    '8em auto 0',
+    '12em auto 0',
+    '15em auto 0',
+  ],
+  padding: 0,
+  borderRadius: '0.5em',
+  textAlign: 'center',
+  background: `${colors.login_background}`,
+}));
 
 export default function DiariesPage() {
   const history = useHistory();
@@ -12,8 +33,8 @@ export default function DiariesPage() {
   }
 
   return (
-    <div>
+    <Container>
       <DiariesContainer onClickDiary={handleClickDiary} />
-    </div>
+    </Container>
   );
 }
