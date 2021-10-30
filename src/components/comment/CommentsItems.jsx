5a770c4005c81f16cd2e4b92dbe3f5dc6da8fac9
seilylook <seilylook@naver.com>
2021-10-30 17:34:08 +0900

    fix: CommentsList Page css 수정

--- src/components/comment/CommentsItems.jsx
+++ src/components/comment/CommentsItems.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+import mediaquery from '../../styles/mediaquery';
+
+const Layout = styled.ul(mediaquery({
+  width: '100%',
+  margin: 0,
+  padding: 0,
+}));
+
+const Item = styled.li(mediaquery({
+  width: ['5em', '5em', '5em', '5em', '5em', '5em'],
+  height: ['2em', '2em', '2em', '2.4em', '2.5em'],
+  padding: '1em',
+  listStyle: 'none',
+  textAlign: 'center',
+  '& p': {
+    width: ['21em', '20.5em', '25em', '27em', '27em', '26em'],
+    height: ['1em', '1.2em', '1.2em', '1.5em', '1.5em', '1.8em'],
+    margin: '1em auto 0.5em',
+    borderRadius: '0.2em',
+    fontSize: ['0.7em', '0.7em', '0.7em', '1em', '1.2em', '1.4em'],
+  },
+}));
+
 export default function CommentsItems({ comments }) {
   return (
-    <ul>
+    <Layout>
       {comments.map((comment) => (
-        <li key={comment.id}>
+        <Item key={comment.id}>
           <p>{comment.text}</p>
-        </li>
+        </Item>
       ))}
-    </ul>
+    </Layout>
   );
 }
