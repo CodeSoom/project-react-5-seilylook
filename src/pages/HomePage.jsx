b120e937cf603cb4790ac1038d12a25a4cae6c46
seilylook <seilylook@naver.com>
2021-10-31 12:21:07 +0900

    fix: image file edition

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,36 +5,42 @@ import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import mediaquery from '../styles/mediaquery';
 
-const Container = styled.div(mediaquery({
-  margin: '0 auto',
-  width: '90%',
-  justifyContent: 'center',
-}));
+const Container = styled.div(
+  mediaquery({
+    margin: '0 auto',
+    width: '90%',
+    justifyContent: 'center',
+  }),
+);
 
-const List = styled.ul(mediaquery({
-  width: '100%',
-  marginTop: '2em',
-  padding: 0,
-  display: 'flex',
-}));
+const List = styled.ul(
+  mediaquery({
+    width: '100%',
+    marginTop: '2em',
+    padding: 0,
+    display: 'flex',
+  }),
+);
 
-const Item = styled.li(mediaquery({
-  width: ['5em', '5em', '5em', '5em', '5em', '5em'],
-  height: ['2em', '2em', '2em', '2.4em', '2.5em'],
-  margin: '0 2em 0 auto',
-  padding: '0.5em',
-  borderRadius: '0.4em',
-  listStyle: 'none',
-  '& a': {
-    color: '#333',
-    fontSize: ['0.6em', '0.7em', '0.8em', '0.9em', '1.1em', '1.4em'],
-    textDecoration: 'none',
-    '&:hover': {
-      fontWeight: 'bold',
-      color: '#000',
+const Item = styled.li(
+  mediaquery({
+    width: ['5em', '5em', '5em', '5em', '5em', '5em'],
+    height: ['2em', '2em', '2em', '2.4em', '2.5em'],
+    margin: '0 2em 0 auto',
+    padding: '0.5em',
+    borderRadius: '0.4em',
+    listStyle: 'none',
+    '& a': {
+      color: '#333',
+      fontSize: ['0.6em', '0.7em', '0.8em', '0.9em', '1.1em', '1.4em'],
+      textDecoration: 'none',
+      '&:hover': {
+        fontWeight: 'bold',
+        color: '#000',
+      },
     },
-  },
-}));
+  }),
+);
 
 export default function HomePage() {
   return (
